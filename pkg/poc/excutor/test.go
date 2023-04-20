812f13ef990ab7494cd729f11f4940ed56f78e28
yuhao <1278412363@qq.com>
2023-04-20 13:55:46 +0800

    feat():finish excutor poc

--- pkg/poc/excutor/test.go
+++ pkg/poc/excutor/test.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/exec"
+)
 
+func main() {
+	cmd := exec.Command("python3", "test.py")
+	output, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(string(output))
 }
